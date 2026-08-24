2f35f339af825b5411b9eeaf34e313fe6ed21487
Manoela Amaral <manoelaamaralduarte@gmail.com>
2026-08-23 21:28:10 -0300

    beecrowd1046 dia 23/08
    Exercício do dia 23/08/2026

---
+++ beecrowd/beecrowd1046.py
@@ -0,0 +1,12 @@
+c , f = map(int, input().split())
+duracao = 0
+if c > f:
+   duracao = (f+24) - c
+elif f > c:
+    duracao = f - c
+elif c == f:
+    duracao = 24
+
+    
+
+print(f'O JOGO DUROU {duracao} HORA(S)')
