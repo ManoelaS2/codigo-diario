d7bbc20fa6cfcd62de103c56f5a32ce358954387
Manoela Amaral <manoelaamaralduarte@gmail.com>
2026-09-01 10:39:13 -0300

    beecrowd1051 e beecrowd1052 dia 01/09
    Exercícios do dia 01/09/2026

---
+++ beecrowd/beecrowd1052.py
@@ -0,0 +1,28 @@
+M = int(input())
+
+if M == 1:
+    mes = "January"
+elif M == 2:
+    mes = "February"
+elif M == 3:
+    mes = "March"
+elif M == 4:
+    mes = "April"
+elif M == 5:
+    mes = "May"
+elif M == 6:
+    mes = "June"
+elif M == 7:
+    mes = "July"
+elif M == 8:
+    mes = "August"
+elif M == 9:
+    mes = "September"
+elif M == 10:
+    mes = "October"
+elif M == 11:
+    mes = "November"
+else:
+    mes = "December"
+
+print(mes)
